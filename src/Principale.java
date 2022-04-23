6e6f93b2235e57a97549544c6e73e9f760b69792
anton <anton@Surf>
2022-04-23 11:49:29 +0200

    versione 1

---
+++ src/Principale.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author antonio
+ * 
+ * @version 1.0
+ *
+ */
+
+public class Principale {
+
+	public static void main(String[] args) {
+		System.out.println("prova gitHub 1");
+
+	}
+
+}
